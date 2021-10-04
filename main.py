b88b1017c9051a0015a16fa05681d849cbb9e5f9
beccakosey22 <820512@mystma.org>
2021-10-04 09:32:21 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+#Create a function called hasL() that returns true/false if a given word w contains at least once instance of the letter "l"
+def hasL(w):
+    for i in range(0, len(w)):
+        if w[i] == "l":
+            return True
+    else:
+        return False
+
+print(hasL("hello"))
+
